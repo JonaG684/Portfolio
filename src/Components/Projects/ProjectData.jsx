7074d66c1b3ee6e7c0a486fe0a363dcf71526c67
Jonathan Garcia <josueavilez54@gmail.com>
2024-10-16 18:00:48 -0300

    Agregue proyecto mas recientes

--- src/Components/Projects/ProjectData.jsx
+++ src/Components/Projects/ProjectData.jsx
@@ -1,6 +1,7 @@
 import RyMLImage from "../../assets/RyML.png";
 import Dogs from "../../assets/Dogs.png";
 import GSC from "../../assets/GSC.png"
+import Edu from "../../assets/Edu.png"
 
 const projectsData = [
   {
@@ -19,6 +20,16 @@ const projectsData = [
     description: "This project as part of my bootcamp knowledge assessment was developed with React and Vite using Styled-Components for the front-end consuming a Dogs API to show the dog breeds and their details applying Express JS with Node on the back-end and its database with Sequelize and SQL.\nDeploy: Coming soon",
     imageUrl: Dogs,
   },
-];
+  {
+    title: "EduPassport",
+    description: (
+      <>
+        Engineered back-end solutions using Python and Flask, focusing on the development and maintenance of RESTful APIs. Designed and managed application routing, ensuring seamless user experience and efficient data handling. Administered and optimized MongoDB databases, including performing schema migrations to support evolving data models. Contributed to front-end development by integrating user roles and managing application state to ensure consistent context throughout the user interface. Led the deployment and maintenance of the application infrastructure on AWS EC2, using Docker for containerization and NGINX for load balancing and reverse proxy management. Collaborated closely with cross-functional teams in an Agile environment, contributing to the full software development lifecycle (SDLC).{" "}
+        <a href="https://www.edupassportio.me/login" target="_blank" rel="noopener noreferrer">See More</a>.
+      </>
+    ),
+    imageUrl: Edu,
+  },
+]
 
 export default projectsData;
