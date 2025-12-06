3db2647fa51aeb858e9d9ea027293e20077fe2fc
Jonathan Garcia <josueavilez54@gmail.com>
2025-12-06 19:16:14 -0300

    revert

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { Element, Link } from "react-scroll";
 import Floating from "../Link/Floating";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const HomeContainer = styled.div`
   min-height: 100vh;
@@ -116,7 +115,6 @@ const Home = () => {
   return (
     <HomeContainer id="home">
       <Floating />
-      <SpeedInsights />
       <Heading>HEY, I'M JONATHAN GARCIA</Heading>
       <Subtitle>
         Full Stack Developer - front-end and graphic design enthusiast.
